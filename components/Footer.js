9fc8f0594a352ac8c8a122c7308454080544b658
sunansheng <snans@163.com>
2021-11-14 13:49:45 +0800

    fix bug

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = ({ fullWidth = true }) => {
     <footer
       className='bg-gray-800 dark:bg-black dark:border-gray-900 border-t flex-shrink-0 justify-center text-center m-auto w-full text-gray-400 text-sm p-6'
     >
-      <span className='fa fa-copyright leading-6'> {` ${y}`} <span> <a href='https://sunansheng.com' className='underline font-bold text-gray-100'>tangly1024.com</a>. Powered by <a href='https://notion.so' className='underline font-bold text-gray-100'>Notion</a> </span>
+      <span className='fa fa-copyright leading-6'> {` ${y}`} <span> <a href='https://sunansheng.com' className='underline font-bold text-gray-100'>tangly1024.com</a>. Powered by <a href='https://notion.so' className='underline font-bold text-gray-100'>Notion</a>.</span> </span>
       <br />
       <span className='fa fa-shield leading-6 mr-2'> <a href='https://beian.miit.gov.cn/' className='ml-1 font-bold'>闽ICP备16032017号-1</a></span>
 
